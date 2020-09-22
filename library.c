9c071f3c0398652d5e329b505a26bf8c547fa18d
Lyubo <lyubo.karev@gmail.com>
2020-09-22 18:27:19 +0300

    Bug fixes on main code

--- library.c
+++ library.c
@@ -3,11 +3,10 @@
 #include <stdio.h>
 #include <math.h>
 uint traverse_graph(char** graph, struct Point* start, const struct Point* bottom_right) {
-    struct Queue visited;
+    struct Queue visited = create_queue();
     uint lower_to_upper = 'a' - 'A';
     char target = graph[start->x][start->y];
-
-    graph[start->x][start->y] -= lower_to_upper; //Marks it as visited
+    graph[start->x][start->y] += lower_to_upper; //Marks it as visited
 
     if(start->x < bottom_right->x && graph[start->x + 1][start->y] == target){
         push(&visited, create_point(start->x + 1, start->y));
@@ -20,12 +19,10 @@ uint traverse_graph(char** graph, struct Point* start, const struct Point* botto
     int length = 1;
     while(visited.length > 0){
         struct Point temp = pop(&visited);
-
         if(graph[temp.x][temp.y] != target){
             continue;
         }
-
-        graph[temp.x][temp.y] -= lower_to_upper;
+        graph[temp.x][temp.y] += lower_to_upper;
         length += 1;
 
         if(temp.x < bottom_right->x && graph[temp.x + 1][temp.y] == target){
@@ -42,6 +39,7 @@ uint traverse_graph(char** graph, struct Point* start, const struct Point* botto
         }
     }
 
+    destroy(&visited);
     return length;
 }
 
@@ -49,10 +47,12 @@ extern uint find_longest_len(char** graph, const struct Point* bottom_right){
     uint max_path = 0;
     for(uint i = 0; i <= bottom_right->x; i++){
         for(uint j = 0; j <= bottom_right->y; j++){
-            struct Point temp = create_point(i, j);
-            uint current_result = traverse_graph(graph, &temp, bottom_right);
-            if(current_result > max_path){
-                max_path = current_result;
+            if(graph[i][j] == 'R' || graph[i][j] == 'G' || graph[i][j] == 'B'){
+                struct Point temp = create_point(i, j);
+                uint current_result = traverse_graph(graph, &temp, bottom_right);
+                if(current_result > max_path){
+                    max_path = current_result;
+                }
             }
         }
     }
